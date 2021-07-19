a940093230709ab9eef7ecfb3db7c48c6c64d006
zukane2 <zukane_402@hotmail.com>
2021-07-20 01:28:58 +0800

    Add files via upload

---
+++ AdjustBreedingLimit.cs
@@ -0,0 +1,45 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using AuthoritativeConfig;
+
+namespace Adjust_Breeding_Limit
+{
+    [BepInPlugin("zukane2.AdjustBreedingLimit", "Adjust Breeding Limit", "2.0.0")]
+    public class AdjustBreedingLimit : BaseUnityPlugin
+    {
+        public new AuthoritativeConfig.Config Config
+        {
+            get { return AuthoritativeConfig.Config.Instance; }
+            set { }
+        }
+
+        private readonly Harmony harmony = new Harmony("zukane2.AdjustBreedingLimit");
+        private static ConfigEntry<int> maxCreatures;
+
+        void Awake()
+        {
+            Config.init(this, true);
+            maxCreatures = Config.Bind<int>("General", "MaxCreatures", 5, "Max Creatures");
+
+            harmony.PatchAll();
+        }
+
+        void OnDestroy()
+        {
+
+            harmony.UnpatchSelf();
+        }
+
+        [HarmonyPatch(typeof(Procreation), "Awake")]
+        static class Breed_Awake_Patch
+        {
+            static void Postfix(ref int ___m_maxCreatures)
+            {
+                ___m_maxCreatures = maxCreatures.Value;
+
+            }
+        }
+
+    }
+}
